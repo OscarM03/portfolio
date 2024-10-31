e0f974f92fb1a982c87cf097925ba53756f14c46
OscarM03 <oscarmutwiri03@gmail.com>
2024-10-31 18:16:22 +0300

    updating thr resume link

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <h1 className="mt-1 text-secondary text-lg">Software Engineer based in Kenya</h1>
             <p className="mt-2 text-[16px]">I love developing user-friendly web applications with a focus on tackling new challenges and refining my skills</p>
             <div className="mt-3 flex items-center gap-4 justify-center">
-                <Button label="View CV" icon={Send} href="https://drive.google.com/file/d/1bcKPsZfsYpBJiFNvOASxhpxDELX5IhbX/view?usp=sharing"/>
+                <Button label="View CV" icon={Send} href="https://drive.google.com/file/d/1Q24Wve4sq-_K9lF8R2UwlSIT5Nr8DhXR/view?usp=sharing"/>
                 <a href="https://www.linkedin.com/in/oscarm-906099281"><img src={Linkedin} alt="Linkedin" width={16}/></a>
                 <a href="https://x.com/OscarM003"><img src={Twitter} alt="Twitter" width={16}/></a>
                 <a href="https://github.com/OscarM03"><img src={Github} alt="Github" width={16}/></a>
