a6e77747e241cc7f5aada1956ac8eec41df4c95b
OscarM03 <oscarmutwiri03@gmail.com>
2024-10-31 16:47:03 +0300

    Changing my description

--- portfolio/src/sections/Hero.jsx
+++ portfolio/src/sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className="mx-28 max-lg:mx-16 max-sm:mx-8  mt-40">
         <div className="text-center">
             <h1 className="text-5xl font-bold max-sm:text-4xl">Hello, I'm Oscar Mutwiri</h1>
-            <h1 className="mt-1 text-secondary text-lg">Web Developer based in Kenya</h1>
+            <h1 className="mt-1 text-secondary text-lg">Software Engineer based in Kenya</h1>
             <p className="mt-2 text-[16px]">I love developing user-friendly web applications with a focus on tackling new challenges and refining my skills</p>
             <div className="mt-3 flex items-center gap-4 justify-center">
                 <Button label="View CV" icon={Send} href="https://drive.google.com/file/d/1bcKPsZfsYpBJiFNvOASxhpxDELX5IhbX/view?usp=sharing"/>
