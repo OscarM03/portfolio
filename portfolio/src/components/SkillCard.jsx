3116eeb6cd4a7deabae56c21877390c4b7db1d30
OscarM03 <oscarmutwiri03@gmail.com>
2024-10-31 16:53:09 +0300

    Adding an hover effect of scale on skills cards

--- portfolio/src/components/SkillCard.jsx
+++ portfolio/src/components/SkillCard.jsx
@@ -1,6 +1,6 @@
 const SkillCard = ({ name, icon }) => {
     return (
-      <div className="flex flex-col items-center justify-center p-1 gap-1 border border-gray-200 rounded-lg shadow-md w-25 h-16 bg-white">
+      <div className="flex flex-col items-center justify-center p-1 gap-1 border border-gray-200 rounded-lg shadow-md w-25 h-16 bg-white transform transition-transform duration-200 hover:scale-105">
         <img src={icon} alt={`${name} icon`} width={25}/>
   
         <div className="relative w-full overflow-hidden h-6 text-center text-gray-700 text-sm">
