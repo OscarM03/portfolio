e115992c19653e60c125d331c919b271af460db7
OscarM03 <oscarmutwiri03@gmail.com>
2024-10-31 16:36:49 +0300

    Adding the skillcard file for the skills design section

---
+++ portfolio/src/components/SkillCard.jsx
@@ -0,0 +1,16 @@
+const SkillCard = ({ name, icon }) => {
+    return (
+      <div className="flex flex-col items-center justify-center p-1 gap-1 border border-gray-200 rounded-lg shadow-md w-25 h-16 bg-white">
+        <img src={icon} alt={`${name} icon`} width={25}/>
+  
+        <div className="relative w-full overflow-hidden h-6 text-center text-gray-700 text-sm">
+          <span className="inline-block whitespace-nowrap">
+            {name}
+          </span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default SkillCard;
+  
